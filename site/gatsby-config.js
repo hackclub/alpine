dfc4c4b0cf21be9495195a9a0067bd3238e07a32
mooghoo <albertopablito8@gmail.com>
2023-06-04 18:51:35 -0600

    test config

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -15,13 +15,6 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
     `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-omni-font-loader`,
