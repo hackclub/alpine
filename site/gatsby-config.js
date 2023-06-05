64348222441a292151f1c028a178231333ebec87
mooghoo <albertopablito8@gmail.com>
2023-06-04 19:08:09 -0600

    reset

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -1,27 +1,10 @@
-
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
   siteMetadata: {
     title: `Alpine`,
-    description: `A Hack Club service event with the Girl Scouts of Utah.`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [
-    `gatsby-transformer-sharp`,
-    {
-      resolve: `gatsby-omni-font-loader`,
-      options: {
-        enableListener: true,
-        preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
-        web: [
-          {
-            name: `Manrope`,
-            file: `https://fonts.googleapis.com/css2?family=Manrope:wght@400;700&display=swap"`,
-          },
-        ],
-      },
-    }
-  ],
+  plugins: [],
 }
